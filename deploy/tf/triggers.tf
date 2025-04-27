4c992de9d51ff647bb29200c4d00ee95ab1b63c8
Dazbo <darren_lester@epam.com>
2025-04-27 17:01:07 +0100

    Add new trigger and cloudbuild-tf

--- deploy/tf/triggers.tf
+++ deploy/tf/triggers.tf
@@ -1,9 +1,10 @@
-resource "google_cloudbuild_trigger" "cd_pipeline" {
+# Trigger for application deployment to Cloud Run
+resource "google_cloudbuild_trigger" "app_cicd_trigger" {
   project = var.project_id
   location = var.cb_region # CB has quote restrictions in certain regions
   name    = "deploy-video-intelligence"
   service_account = resource.google_service_account.cicd_runner_sa.id
-  description = "Trigger for ${var.trigger_branch_name} branch deployment"
+  description = "Trigger for ${var.trigger_branch_name} application deployment"
 
   repository_event_config {
     repository = google_cloudbuildv2_repository.repo.id
@@ -16,7 +17,6 @@ resource "google_cloudbuild_trigger" "cd_pipeline" {
   included_files = [
     "src/**",
     "tests/**",
-    "deploy/**"
   ]
 
   ignored_files   = ["README.md"]
@@ -30,19 +30,70 @@ resource "google_cloudbuild_trigger" "cd_pipeline" {
     _SERVICE_NAME                  = var.service_name
     _LOG_LEVEL                     = var.log_level
     _MAX_INSTANCES                 = "1"
-    _CICD_RUNNER_SA_NAME           = "${var.cicd_runner_sa_name}@${var.project_id}.iam.gserviceaccount.com"
+    _CICD_RUNNER_SA_EMAIL           = "${var.cicd_runner_sa_name}@${var.project_id}.iam.gserviceaccount.com"
   }
 
   depends_on = [resource.google_project_service.apis, google_cloudbuildv2_repository.repo]
 
   tags = [
-    "terraform-managed",
     var.service_name, 
     var.trigger_branch_name # Tag with the branch/environment
   ]
 
   lifecycle {
     # Prevent accidental deletion if the trigger is manually modified
+    prevent_destroy = false # Set to true in production if desired
+  }
+}
+
+# Trigger for infrastructure deployment using Terraform
+resource "google_cloudbuild_trigger" "tf_trigger" {
+  project         = var.project_id
+  location        = var.cb_region # Trigger must be in the same region as the repo connection
+  name            = "apply-terraform-infra"
+  service_account = resource.google_service_account.cicd_runner_sa.id
+  description     = "Trigger for ${var.trigger_branch_name} Terraform infrastructure deployment"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.repo.id
+    push {
+      branch = "^${var.trigger_branch_name}$"
+    }
+  }
+
+  filename       = "deploy/cloudbuild-tf.yaml" # Point to the Terraform Cloud Build file
+  included_files = [
+    "deploy/**", # Trigger only on changes within the deploy directory
+  ]
+
+  ignored_files = ["README.md"] # Ignore other folders
+
+  # Define substitutions required by cloudbuild-tf.yaml
+  substitutions = {
+    _TF_STATE_BUCKET              = "${var.project_id}-tfstate"
+    _DEPLOY_REGION                = var.region
+    _CB_REGION                    = var.cb_region
+    _ORG                          = var.my_org
+    _SERVICE_NAME                 = var.service_name
+    _ARTIFACT_REPO_NAME           = var.artifact_repo_name
+    _REPO_NAME                    = var.repo_name
+    _REPO_OWNER                   = var.repo_owner
+    _GITHUB_APP_INSTALLATION_ID   = var.github_app_installation_id
+    _GITHUB_PAT_SECRET_ID         = var.github_pat_secret_id
+    _BRANCH                       = var.trigger_branch_name
+    _LOG_LEVEL                    = var.log_level
+    _CICD_RUNNER_SA_EMAIL         = "${var.cicd_runner_sa_name}@${var.project_id}.iam.gserviceaccount.com"
+  }
+
+  depends_on = [resource.google_project_service.apis, google_cloudbuildv2_repository.repo]
+
+  tags = [
+    "terraform-managed",
+    "infra-deployment",
+    var.trigger_branch_name # Tag with the branch/environment
+  ]
+
+  lifecycle {
     prevent_destroy = false # Set to true in production if desired
   }
 }
